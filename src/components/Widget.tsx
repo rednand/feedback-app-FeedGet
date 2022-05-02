8eae1771afd22b62ecd499c0e01b324445fbb063
rednand <nan-aguiar@hotmail.com>
2022-05-02 12:11:29 -0300

    project init

---
+++ src/components/Widget.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { ChatTeardropDots } from "phosphor-react";
+import { Popover } from "@headlessui/react";
+
+export function Widget() {
+  return (
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>HELLO WORLD</Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-6 h-6" />
+        <div className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2">Feedback</span>
+        </div>
+      </Popover.Button>
+    </Popover>
+  );
+}
